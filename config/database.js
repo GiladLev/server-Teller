68e0aafbafb934d8a45f9d43b8a54fdae8feaf6d
GiladLev <giladlev2000@gmail.com>
2023-02-17 10:44:00 +0200

    env

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cfnjd64gqg415e2s7li0-a.frankfurt-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'sample_zw1p'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'RJzpcLz5BdkJRWohQsu31WNPSDaMJkmp'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
